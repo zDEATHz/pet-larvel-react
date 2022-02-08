40dd54b37819e34af26d625c39098e908f91633d
zDEATHz <zdeathz@gmail.com>
2022-02-08 20:40:12 +0000

    init

---
+++ resources/js/App.js
@@ -0,0 +1,73 @@
+import React, {useState, useRef, useMemo} from 'react';
+import ReactDOM from 'react-dom';
+import '../css/app.css';
+import PostList from "./components/PostList";
+import PostForm from "./components/PostForm";
+import PostFilter from "./components/PostFilter";
+import MySelect from "./components/UI/select/MySelect";
+import MyInput from "./components/UI/input/MyInput";
+import MyModal from "./components/UI/MyModal/MyModal";
+import MyButton from "./components/UI/button/MyButton";
+
+
+function App() {
+
+	const [posts, setPosts] = useState([
+			{id: 1, title: 'js', body: 'description'},
+			{id: 2, title: '2js2', body: '2description2'},
+			{id: 3, title: '3js3', body: '3description3'},
+	])
+	const [filter, setFilter] = useState({sort: '', query: ''});
+	const [modal, setModal] = useState(false);
+
+	const sortedPosts = useMemo( () => {
+		if(filter.sort){
+			return [...posts].sort( (a, b) => a[filter.sort].localeCompare(b[filter.sort]))
+		}
+		return posts;
+	}, [filter.sort, posts])
+
+	const sortedAndSearchedPosts = useMemo( () => {
+		return sortedPosts.filter(post => post.title.toLowerCase().includes(filter.query))
+	}, [filter.query, sortedPosts])
+
+	const createPost = (newPost) => {
+		setPosts([...posts, newPost])
+		setModal(false)
+	}
+
+	const removePost = (post) => {
+		setPosts(posts.filter(p => p.id !== post.id))
+	}
+
+
+
+
+	return (
+		<div className="App">
+			<MyButton style={{marginTop: 30}} onClick={() => setModal(true)}>
+				Создать пост
+			</MyButton>
+			<MyModal visible={modal} setVisible={setModal}>
+				<PostForm create={createPost}/>
+			</MyModal>
+			
+			<hr style={{margin: '15px 0'}}/>
+			<PostFilter
+				filter={filter}
+				setFilter={setFilter}
+			 />
+			<PostList remove={removePost} posts={sortedAndSearchedPosts} title="Список постов 1" />
+				
+			
+			
+
+		</div>
+	);
+}
+
+export default App;
+
+//if (document.getElementById('app')) {
+//    ReactDOM.render(<App />, document.getElementById('app'));
+//}
---
+++ resources/js/index.js
@@ -0,0 +1,14 @@
+require('./bootstrap');
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+import Example from './components/Example';
+
+ReactDOM.render(
+    <div>
+  
+    	<App />
+    </div>,
+    document.querySelector('#root')
+);
